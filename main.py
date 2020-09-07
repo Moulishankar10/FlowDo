a96dd57d1b38334d925b3bc5af0f6f748bc7c408
Vigneshwar K R <vicky.pcbasic@gmail.com>
2020-09-07 12:07:30 +0530

    Added Tk window

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+# Required modules
 import tkinter as tk
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import pyglet as pyg
+
+# Main Menu
+root = tk.Tk()
+root.title("FlowDo")
+root.geometry("1366x768")
+root.mainloop()
