d43aa2c9a90dff7e9eb835620a43f4a454303b07
Moulishankar M R <68773466+Moulishankar10@users.noreply.github.com>
2020-09-12 20:41:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def orderOptionsVisualizer():
 def Order():
     orderOptionsVisualizer()
     while(True):
-        opt = int(input("Enter your option: "))
+        opt = int(input("Enter your option : "))
         if opt == 0:
             orderOptionsVisualiser()
             continue
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     inv_table,prod_code,prod_name,avail_stock,max_stock,revenue = dataImporter()
     mainOptionsVisualizer()
     while(1):
-        option = int(input("Enter an option:"))
+        option = int(input("Enter your option :"))
         if option == 0:
             mainOptionsVisualizer()
             break
