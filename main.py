89d307e02472c272701c410032b06673862f1574
Moulishankar M R <moulishankarmr@gmail.com>
2020-10-22 16:24:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,6 +56,11 @@ def inventoryOptionsVisualizer():
     print("Press 9 for exit.")  
     print("\n**********************************************************************************************\n")
 
+def revMonthChecker():
+    today = datetime.today()
+    frmt = "{}-{}".format(today.month,today.year)
+    rev_data = pd.read_csv('data/revenue.csv')
+    
 # CLASS FOR BILLING OPERATIONS
 class Biller:
     def __init__(self,l):
