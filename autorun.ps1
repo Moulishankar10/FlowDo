776fbcd45779bc4cc05d6245c408e7cdc63a9df3
Moulishankar10 <moulishankarmr@gmail.com>
2020-12-02 18:11:23 +0530

    Modified the powershell script

--- autorun.ps1
+++ autorun.ps1
@@ -1,2 +1,17 @@
 Write-Output "FlowDo Autorun Script for Windows"
 Write-Output "Checking for Python installation"
+$res = Read-Host "Have you installed Python? (Y/N)"
+if ( $res -eq "N" -OR "n")
+{
+    Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe
+    ./python-3.9.0-amd64.exe /quiet
+}
+
+Write-Output "Checking the Dependencies...."
+
+pip install numpy
+pip install pandas
+pip install datetime
+pip install matplotlib
+
+Write-Output "Now, You are all set!"
