36b4eb8cec6eb076b4e6d3dd61f166245f8eac4f
Vigneshwar Ravichandar <vicky.pcbasic@gmail.com>
2020-12-02 18:36:05 +0530

    minor changes

--- autorun.ps1
+++ autorun.ps1
@@ -1,10 +1,15 @@
+
 Write-Output "FlowDo Autorun Script for Windows"
 Write-Output "Checking for Python installation"
 $res = Read-Host "Have you installed Python? (Y/N)"
 if ( $res -eq "N" -OR "n")
 {
-    Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe
-    ./python-3.9.0-amd64.exe /quiet
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe" -OutFile "python.exe"
+
+    ./python.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
 }
 
 Write-Output "Checking the Dependencies...."
