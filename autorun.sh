ed909764712a930ca3810d6cabbae570e0e08f92
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-22 19:59:48 +0530

    Updated the shell script

--- autorun.sh
+++ autorun.sh
@@ -3,25 +3,16 @@
 clear
 
 echo "FlowDo Autorun Script"
-echo ""
-echo "Checking for Python Installation..."
+echo " "
 
-if which python3 >/dev/null;
-then
-  echo "Python 3 is already installed."
-else
-  echo "Python isn't found."
-  echo "Installing Python 3..."
-  sudo apt install python3 -y
-  echo "Successfully installed Python 3."
-fi
 
+echo "Checking for pip3 installation ..."
 if which pip3 >/dev/null;
 then
   echo "pip3 is already installed."
 else
   echo "pip3 isn't found."
-  echo "Installing pip3..."
+  echo "Installing pip3 ..."
   sudo apt install python3-pip -y
   echo "Successfully installed pip3."
 fi
@@ -29,4 +20,5 @@ fi
 echo "Checking the Dependencies...."
 pip3 install numpy pandas datetime matplotlib
 
+echo " "
 echo "Now, You are all set!"
