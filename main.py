143b404a4cd44aedefa668cd11c9dc524778a9c9
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-30 20:32:01 +0530

    MOdified the code

--- main.py
+++ main.py
@@ -374,8 +374,7 @@ def Order():
                     print("\n!!! Sorry, It is an Empty Bill !!!\n")
                 else:
                     print("\n------------------------- BILL --------------------------\n")
-                    print("\n=========================================================\n")
-                    print(f"Bill Date : {now.strftime('%d-%m-%Y     %I:%M %p')}")
+                    print(f"\nBill Date : {now.strftime('%d-%m-%Y     %I:%M %p')}")
                     print("\n=========================================================\n")
                     b.display()
             
